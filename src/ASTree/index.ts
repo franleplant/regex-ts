4d993531e61fcd6f6a4befcd79002c59d0c82eda
Fran Guijarro <franleplant@gmail.com>
2020-05-03 16:22:31 -0300

    improve the ASTree interface for more ergonomic tree manipulation

--- src/ASTree/index.ts
+++ src/ASTree/index.ts
@@ -34,7 +34,74 @@ export default class ASTree {
     this.children = children;
   }
 
+  childrenLength(): number {
+    if (this.children) {
+      return this.children.length;
+    }
+
+    return 0;
+  }
+
   isLambda(): boolean {
     return this.kind === "LAMBDA";
   }
+
+  isIntersection(): boolean {
+    return this.kind === "INTERSECTION";
+  }
+
+  isUnion(): boolean {
+    return this.kind === "UNION";
+  }
+
+  popChildIf(predicate: Predicate): ASTree | undefined {
+    if (!this.children || this.children.length === 0) {
+      return;
+    }
+
+    const lastChild = this.children[this.children.length - 1];
+    if (!lastChild) {
+      return;
+    }
+
+    const shouldPop = predicate(lastChild);
+    if (!shouldPop) {
+      return;
+    }
+
+    this.children.pop();
+    return lastChild;
+  }
+
+  getChild(index: number): ASTree | undefined {
+    if (!this.children) {
+      return;
+    }
+
+    return this.children[index];
+  }
+
+  getChildIf(index: number, isChild: Predicate): ASTree | undefined {
+    if (!this.children || this.children.length === 0) {
+      return;
+    }
+
+    const child = this.children[index];
+    if (!child) {
+      return;
+    }
+
+    if (isChild(child)) {
+      return child;
+    }
+    return;
+  }
+
+  // TODO document
+  isChildAt(index: number, isChild: Predicate): boolean {
+    const child = this.getChildIf(index, isChild);
+    return !!child;
+  }
 }
+
+export type Predicate = (child: ASTree) => boolean;
--- src/toAST/index.ts
+++ src/toAST/index.ts
@@ -20,29 +20,17 @@ export default function toAST(tree: ASTree): ASTree {
         "intersection(lit_1, ..., lit_n, intersection(any, lamda)) => intersection(lit_1, ..., lit_n, any)",
         JSON.stringify(tree)
       );
-      // popChild("INTERSECTION", child => child.childrenLength() === 2)
-      // const intersection = tree.getLastChildIf(child => child.isIntersection() && child.getChildIf(1, child => child.isLambda()))
-      //if (intersection) {
-      //tree.popChild()
-      //tree.children = [
-      //...tree.children,
-      //intersection.getChild(0),
-      //]
-      //}
-      const intersectionIndex = tree.children.length - 1;
-      const intersection = tree.children[intersectionIndex];
-      if (
-        intersection &&
-        intersection.kind === "INTERSECTION" &&
-        intersection.children?.length === 2 &&
-        intersection.children?.[1]?.isLambda()
-      ) {
-        tree.children = [
-          ...tree.children.slice(0, intersectionIndex),
-          intersection.children[0],
-        ];
+
+      const intersection = tree.popChildIf(
+        (child) =>
+          child.isIntersection() &&
+          child.isChildAt(1, (child) => child.isLambda())
+      );
+      if (intersection) {
+        tree.children = [...tree.children, intersection.getChild(0)];
         return toAST(tree);
       }
+
       console.log("skipping");
     }
 
@@ -50,33 +38,14 @@ export default function toAST(tree: ASTree): ASTree {
       console.log(
         "intersection(lit_1, ..., lit_n, intersection(lit_n+1, any)) => intersection(lit_1, ..., lit_n, lit_n+1, any)"
       );
-      /*
-        const intersection = tree.getLastChildIf(
-            child => child.isIntersection() && child.childrenLen() >= 2
-        )
-        if (intersection) {
-          tree.children.pop()
-          tree.children = [
-              ...tree.children,
-            ...(intersection.children as Array<ASTree>),
-          ]
-          return toAST(tree)
-        }
-       */
+
       // Should be last
-      const intersectionIndex = tree.children.length - 1;
-      const intersection = tree.children[intersectionIndex];
-      if (
-        intersection &&
-        intersection.kind === "INTERSECTION" &&
-        (intersection.children?.length || 0) >= 2
-      ) {
-        console.log(
-          "child intersection",
-          JSON.stringify(intersection, null, 2)
-        );
+      const intersection = tree.popChildIf(
+        (child) => child.isIntersection() && child.childrenLength() >= 2
+      );
+      if (intersection) {
         tree.children = [
-          ...tree.children.slice(0, intersectionIndex),
+          ...tree.children,
           ...(intersection.children as Array<ASTree>),
         ];
         return toAST(tree);
@@ -85,10 +54,10 @@ export default function toAST(tree: ASTree): ASTree {
     }
 
     if (tree.children.length >= 2) {
-      /*
-      const union = tree.getLastChildIf(child => child.isUnion())
+      console.log("intersection(...children, union(any))");
+      // Should be the last
+      const union = tree.popChildIf((child) => child.isUnion());
       if (union) {
-        tree.children.pop()
         return toAST(
           new ASTree({
             kind: "UNION",
@@ -96,20 +65,7 @@ export default function toAST(tree: ASTree): ASTree {
           })
         );
       }
-
-      */
-      console.log("intersection(...children, union(any))");
-      // Should be the last
-      const unionIndex = tree.children.length - 1;
-      if (tree.children[unionIndex]?.kind === "UNION") {
-        const union = tree.children.pop() as ASTree;
-        return toAST(
-          new ASTree({
-            kind: "UNION",
-            children: [tree, union.children?.[0]],
-          })
-        );
-      }
+      console.log("skipping");
     }
   }
 
